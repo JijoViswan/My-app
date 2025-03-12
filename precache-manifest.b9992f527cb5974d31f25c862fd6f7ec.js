6da855c8773198e8cce9d9e43611dbd2298367fc
jmv72001 <jmv72001@gmail.com>
2025-03-13 02:21:08 +0400

    Updates

---
+++ precache-manifest.b9992f527cb5974d31f25c862fd6f7ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5d739ecb83bc3d819c10e4694c56f1",
+    "url": "/My-app/index.html"
+  },
+  {
+    "revision": "e28d821a4ed52dd817dd",
+    "url": "/My-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/My-app/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/My-app/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "e28d821a4ed52dd817dd",
+    "url": "/My-app/static/js/main.d01ee37b.chunk.js"
+  },
+  {
+    "revision": "8c13bc088d6818068da3",
+    "url": "/My-app/static/js/runtime~main.8087eba0.js"
+  }
+]);
